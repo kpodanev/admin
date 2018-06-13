9086958bd46f78c7da95e268a62d85fd1d0ceb17
unknown <kpodanev@gmail.com>
2018-06-13 23:03:19 +0300

    create

--- articles/create.php
+++ articles/create.php
@@ -1,20 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-</head>
-<body>
 <?php
 $mysqli = mysqli_connect("127.0.0.1", "root", "", "shop");
 
 if (mysqli_connect_errno($mysqli)) {
     echo "Не удалось подключиться" . mysqli_connect_error();
 }
-if(isset($_POST['title']) && isset($_POST['description']) && isset($_POST['created_at'])){
+if(isset($_POST['id']) && isset($_POST['title']) && isset($_POST['description']) && isset($_POST['created_at'])){
+    $id = htmlentities(mysqli_real_escape_string($mysqli, $_POST['id']));
     $title = htmlentities(mysqli_real_escape_string($mysqli, $_POST['title']));
     $description = htmlentities(mysqli_real_escape_string($mysqli, $_POST['description']));
     $created_at = htmlentities(mysqli_real_escape_string($mysqli, $_POST['created_at']));
-    $query ="INSERT INTO articles VALUES(NULL, '$title','$description','$created_at')";
+    $query ="INSERT INTO articles VALUES('id', '$title','$description','$created_at')";
     $result = mysqli_query($mysqli, $query) or die("Ошибка " . mysqli_error($mysqli)); 
     if($result)
     {
@@ -22,17 +17,26 @@ if(isset($_POST['title']) && isset($_POST['description']) && isset($_POST['creat
     }
     
 }
-?>
-<h2>Добавить новую статью</h2>
-<form method="POST">
-<p>Введите название:<br> 
-<input type="text" name="title" /></p>
-<p>Путь: <br> 
-<input type="text" name="description" /></p>
-<input type="submit" value="Добавить">
-</form>
-<?=
 mysqli_close($mysqli);
+
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+    <h2>Добавить новую статью</h2>
+    <form method="POST">
+    <p>ID:<br>
+    <input type="text" name="id" /></p>
+    <p>Введите название:<br> 
+    <input type="text" name="title" /></p>
+    <p>Путь: <br> 
+    <input type="text" name="description" /></p>
+    <p>Дата создания: <br>
+    <input type="text" name="created_at" /></p>
+    <input type="submit" value="Добавить">
+    </form>
 </body>
 </html>
